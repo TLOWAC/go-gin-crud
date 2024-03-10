ce84b76e6de4327562ce73bdc9ecee26050aac0a
TLOWAC <leechanghun55@gmail.com>
2024-03-11 04:41:36 +0900

    chore: router 구성 위치 수정

---
+++ api/routes/book-route.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	service "example/web-service-gin/api/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AddBooksRoutes(rg *gin.RouterGroup) {
+	book := rg.Group("/books")
+	h, _ := service.NewBookServiceImpl()
+	
+	book.GET("", h.GetBooks)
+
+	// content.GET("/:id", h.GetContent)
+	// content.POST("", h.AddContent)
+	// content.PATCH("/:id", h.UpdateContent)
+	// content.DELETE("/:id", h.DeleteContent)
+}
--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"example/web-service-gin/api/routes"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	routes.SetupRoutes("localhost:8080")
+  router := gin.Default()
+    v1 := router.Group("/")
+
+    router.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+
+		routes.AddBooksRoutes(v1)
+		
+    router.Run("localhost:8080")
 }
