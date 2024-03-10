85f503b6c80517a2d0cd54ce97b7f0d5c0be716b
TLOWAC <leechanghun55@gmail.com>
2024-03-10 21:19:10 +0900

    refactor(db): DB connection 재사용 가능하게 로직 수정

--- internal/db/postgresql.go
+++ internal/db/postgresql.go
@@ -1,23 +1,38 @@
 package db
 
 import (
-	"example/web-service-gin/api/models"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func Init() *gorm.DB {
-	dbURL := "postgresql://postgres:root12#@localhost:5432/go-local"
 
-	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+type Postgresql struct {
+	pg *gorm.DB
+}
 
+func NewORM() (*Postgresql, error) {
+
+	// DB_URL 환경 변수 값 조회
+	dbURL := os.Getenv("DB_URL")
+	
+	// DB_URL 이 없는 경우 예외처리 및 초기값 할당
+	if dbURL == "" {
+		dbURL = "postgresql://postgres:root12#@localhost:5432/go-local"
+	}
+
+	// gorm, postgresql 연동
+	pgConn, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+	
 	if err != nil {
 		log.Fatalln(err)
 	}
+	// db.Migrator().AutoMigrate(&models.Book{})
 
-	db.Migrator().AutoMigrate(&models.Book{})
-
-	return db
+	// handler DI 에서 pg orm conn 을 재사용 할 수 있도록 연결값을 담아 반환
+	return &Postgresql {
+		pg: pgConn,
+	}, err
 }
