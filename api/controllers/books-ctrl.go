155c9ec4c32407de00f2dc9364b5c2eeaf75c4b7
TLOWAC <leechanghun55@gmail.com>
2024-03-11 00:20:03 +0900

    feat(controller): book controller 초기 셋팅

---
+++ api/controllers/books-ctrl.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	handler "example/web-service-gin/api/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AddBooksRoutes(rg *gin.RouterGroup) {
+	book := rg.Group("/books")
+	h, _ := handler.NewHandler()
+	
+	book.GET("/", h.GetBooks)
+
+	// content.GET("/:id", h.GetContent)
+	// content.POST("", h.AddContent)
+	// content.PATCH("/:id", h.UpdateContent)
+	// content.DELETE("/:id", h.DeleteContent)
+}
