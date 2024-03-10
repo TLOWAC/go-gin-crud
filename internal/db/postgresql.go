27ee609231416a2d1206683bfdf2b23ebada3a4b
TLOWAC <leechanghun55@gmail.com>
2024-03-11 00:18:54 +0900

    refactor(db): default auth url 변경 및 구조체 필드 수정

--- internal/db/postgresql.go
+++ internal/db/postgresql.go
@@ -10,17 +10,17 @@ import (
 
 
 type Postgresql struct {
-	pg *gorm.DB
+	*gorm.DB
 }
 
-func NewORM() (*Postgresql, error) {
+func NewPgManager() (*Postgresql, error) {
 
 	// DB_URL 환경 변수 값 조회
 	dbURL := os.Getenv("DB_URL")
 	
 	// DB_URL 이 없는 경우 예외처리 및 초기값 할당
 	if dbURL == "" {
-		dbURL = "postgresql://postgres:root12#@localhost:5432/go-local"
+		dbURL = "postgres://root:root123@localhost:5432/go-local"
 	}
 
 	// gorm, postgresql 연동
@@ -33,6 +33,6 @@ func NewORM() (*Postgresql, error) {
 
 	// handler DI 에서 pg orm conn 을 재사용 할 수 있도록 연결값을 담아 반환
 	return &Postgresql {
-		pg: pgConn,
+		pgConn,
 	}, err
 }
