fe9506329597efa63b5ef43e937865ef5126d7f3
TLOWAC <leechanghun55@gmail.com>
2024-03-11 21:36:53 +0900

    chore: 공백 제거

--- internal/db/postgresql.go
+++ internal/db/postgresql.go
@@ -33,7 +33,5 @@ func NewPgManager() (*Postgresql, error) {
 	pgConn.Migrator().AutoMigrate(&models.Book{})
 
 	// handler DI 에서 pg orm conn 을 재사용 할 수 있도록 연결값을 담아 반환
-	return &Postgresql {
-		pgConn,
-	}, err
+	return &Postgresql{pgConn}, err
 }
